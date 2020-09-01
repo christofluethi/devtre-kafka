deb5be048aa4fc311d92458112e5c1180721a937
cluethi <luethi@puzzle.ch>
2020-09-01 14:54:07 +0200

    tweet transformer

---
+++ twitter-data-transformer/src/main/java/ch/puzzle/devtre/test/control/TwitterDataTransformer.java
@@ -0,0 +1,19 @@
+package ch.puzzle.devtre.test.control;
+
+import ch.puzzle.devtre.test.entity.Tweet;
+import io.smallrye.reactive.messaging.annotations.Broadcast;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class TwitterDataTransformer {
+
+    @Broadcast
+    @Outgoing("tweets")
+    @Incoming("twitter-inbound")
+    public String transform(Tweet tweet) {
+        return tweet.getText();
+    }
+}
---
+++ twitter-data-transformer/src/main/java/ch/puzzle/devtre/test/entity/Tweet.java
@@ -0,0 +1,14 @@
+package ch.puzzle.devtre.test.entity;
+
+public class Tweet {
+
+    public String Text;
+
+    public String getText() {
+        return Text;
+    }
+
+    public void setText(String text) {
+        Text = text;
+    }
+}
