7df4a7d91813c6ded71b6d732dcbd915519bfa78
rhertle <hertle@puzzle.ch>
2020-09-01 15:52:21 +0200

    Add logging to transformer

--- manual-data-transformer/src/main/java/ch/puzzle/devtre/manualdatatransformer/boundary/ManualDataTransformer.java
+++ manual-data-transformer/src/main/java/ch/puzzle/devtre/manualdatatransformer/boundary/ManualDataTransformer.java
@@ -5,14 +5,18 @@ import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.logging.Logger;
 
 @ApplicationScoped
 public class ManualDataTransformer {
+
+    private static final Logger LOGGER = Logger.getLogger(ManualDataTransformer.class.getName());
 
     @Incoming("manual")
     @Outgoing("manual-transformed")
     @Broadcast
     public String transform(String value) {
+        LOGGER.info("Received message: " + value);
         return value.toUpperCase();
     }
 }
