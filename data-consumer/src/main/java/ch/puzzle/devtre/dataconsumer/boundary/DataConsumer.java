ba65c9c668ea5afce9329d131e022f84dbe74f0a
rhertle <hertle@puzzle.ch>
2020-09-02 08:41:17 +0200

    Add rest endpoint for streaming tweets

--- data-consumer/src/main/java/ch/puzzle/devtre/dataconsumer/boundary/DataConsumer.java
+++ data-consumer/src/main/java/ch/puzzle/devtre/dataconsumer/boundary/DataConsumer.java
@@ -1,23 +1,45 @@
 package ch.puzzle.devtre.dataconsumer.boundary;
 
+import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import org.jboss.resteasy.annotations.SseElementType;
+import org.reactivestreams.Publisher;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import java.util.logging.Logger;
 
-@ApplicationScoped
+@Path("/data")
 public class DataConsumer {
 
     private static final Logger LOGGER = Logger.getLogger(DataConsumer.class.getName());
 
+    @Inject
+    @Channel("tweets-stream")
+    Publisher<String> tweetsPublisher;
+
     @Incoming("tweets")
-    public void streamTweets(String tweet) {
+    @Outgoing("tweets-stream")
+    public String streamTweets(String tweet) {
         LOGGER.info(tweet);
+        return tweet;
     }
 
     @Incoming("manual-transformed")
     public void streamData(String data) {
         LOGGER.info(data);
+    }
+
+    @GET
+    @Path("/tweets/stream")
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    @SseElementType("text/plain")
+    public Publisher<String> streamTweets() {
+        return tweetsPublisher;
     }
 }
