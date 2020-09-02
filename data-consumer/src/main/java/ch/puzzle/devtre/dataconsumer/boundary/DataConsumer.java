1e2a7f66c55c49ac44f940c9202a1684d2803fbc
cluethi <luethi@puzzle.ch>
2020-09-02 22:11:41 +0200

    DataConsumer publishing multiple topics to a single websocket

--- data-consumer/src/main/java/ch/puzzle/devtre/dataconsumer/boundary/DataConsumer.java
+++ data-consumer/src/main/java/ch/puzzle/devtre/dataconsumer/boundary/DataConsumer.java
@@ -1,25 +1,30 @@
 package ch.puzzle.devtre.dataconsumer.boundary;
 
+import io.smallrye.reactive.messaging.kafka.IncomingKafkaRecordMetadata;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Message;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
 import javax.enterprise.context.ApplicationScoped;
-import java.util.logging.Logger;
+import java.util.Optional;
 
 @ApplicationScoped
 public class DataConsumer {
 
-    private static final Logger LOGGER = Logger.getLogger(DataConsumer.class.getName());
-
     @Incoming("manual-transformed")
-    public void streamData(String data) {
-        LOGGER.info(data);
+    @Outgoing("websocket-broadcast")
+    public String streamManual(Message<String> msg) {
+        return getKafkaMetadata(msg)+": "+msg.getPayload();
     }
 
     @Incoming("tweets")
-    @Outgoing("tweets-stream")
-    public String streamTweets(String data) {
-        LOGGER.info(data);
-        return data;
+    @Outgoing("websocket-broadcast")
+    public String streamTweets(Message<String> msg) {
+        return getKafkaMetadata(msg)+": "+msg.getPayload();
+    }
+
+    String getKafkaMetadata(Message<String> msg) {
+        Optional<IncomingKafkaRecordMetadata> meta = msg.getMetadata(IncomingKafkaRecordMetadata.class);
+        return meta.map(m -> m.getTopic()+"-"+m.getPartition()+" offset "+m.getOffset()).orElse("unknown");
     }
 }
